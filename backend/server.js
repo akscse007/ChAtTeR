1b1ecddbb6dc831ac8e3c2329596b26865ed188a
akscse007 <roy.akashcucse@gmail.com>
2026-01-10 04:32:25 +0530

    Fix: Express catch-all route for production

--- backend/server.js
+++ backend/server.js
@@ -30,14 +30,17 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
+  app.get("/*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname1, "frontend", "build", "index.html")
+    );
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running...");
   });
 }
+
 // ---------------- DEPLOYMENT ----------------
 
 // Error handling
