ba8f5f437d5a557e421a83a1619764b64a17230c
akscse007 <roy.akashcucse@gmail.com>
2026-01-10 06:40:43 +0530

    Fix API baseURL

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
-  withCredentials: true,
+  baseURL: "https://chatter-backend-9ujs.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
